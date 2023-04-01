ef9b5de6bf338916b47016c1f99d972c29a233d8
Lucas Pulsz <lucaspulsz@gmail.com>
2023-03-31 23:54:16 -0300

    Solving puzzle 2

--- project_1/knights/puzzle.py
+++ project_1/knights/puzzle.py
@@ -40,7 +40,19 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # initial condition
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # same kind
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+
+    # different kinds
+    Implication(BKnight, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnave, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
 )
 
 # Puzzle 3
